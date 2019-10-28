129df8aa48ce1c6effb104b2531e637fc6f16b21
xia0m <alexma325@gmail.com>
2019-10-27 22:46:39 -0700

    changed cart-icon scss to styled component

--- src/components/cart-icon/index.jsx
+++ src/components/cart-icon/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.action";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { createStructuredSelector } from "reselect";
 
-import "./style.scss";
+import {
+  CartIconContainer,
+  ShoppingIconContainer,
+  ItemCountContainer
+} from "./cart-icon.style";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{itemCount}</span>
-  </div>
+  <CartIconContainer onClick={toggleCartHidden}>
+    <ShoppingIconContainer />
+    <ItemCountContainer>{itemCount}</ItemCountContainer>
+  </CartIconContainer>
 );
 
 const mapStateToProps = state =>
