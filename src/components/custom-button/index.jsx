a5da4232c4a744df3508d51e50e99fc56fce3327
xia0m <alexma325@gmail.com>
2019-10-27 22:35:50 -0700

    changed style of cart-dropdown to styled component

--- src/components/custom-button/index.jsx
+++ src/components/custom-button/index.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import { CustomButtonContainer } from "./custom-button.style";
 
-import "./style.scss";
-
 const CustomButton = ({ children, ...props }) => (
   <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
